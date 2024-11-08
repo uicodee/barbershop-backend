61b0933bdac63976d8bea0a8fb0712c54d4aaedc
uicode <uicodee@gmail.com>
2024-11-08 20:13:46 +0500

    upd

--- app/domain/data.py
+++ app/domain/data.py
@@ -2,7 +2,9 @@ from apscheduler.jobstores.redis import RedisJobStore
 
 job_stores = {
     "default": RedisJobStore(
-        jobs_key="scheduled_haircuts_jobs", run_times_key="scheduled_haircuts_run_times",
-        host="localhost", port=6379
+        jobs_key="scheduled_haircuts_jobs",
+        run_times_key="scheduled_haircuts_run_times",
+        host="redis",
+        port=6379
     )
 }
