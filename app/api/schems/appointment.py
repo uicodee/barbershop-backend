0633bfc503c666fe729f440c801a0a7fa61ecf6c
uicode <uicodee@gmail.com>
2024-11-14 23:53:19 +0500

    upd

--- app/api/schems/appointment.py
+++ app/api/schems/appointment.py
@@ -11,14 +11,15 @@ class Appointment(BaseModel):
     def validate_and_convert_date(cls, value):
         if isinstance(value, str):
             try:
-                datetime.strptime(value, "%d.%m.%Y %H:%M")
+                date = datetime.strptime(value, "%d.%m.%Y %H:%M")
+                if date < datetime.now():
+                    raise ValueError("Date should be in the future")
             except ValueError:
                 raise ValueError("Incorrect date format, should be 'DD.MM.YYYY HH:MM'")
         return value
 
 
 class UpdateAppointment(BaseModel):
-
     appointment_date: str = Field(alias="appointmentDate")
 
     @field_validator("appointment_date", mode="before")
