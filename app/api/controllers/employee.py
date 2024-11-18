5275c6661bf32e214ac24c220636e54798f54510
uicode <uicodee@gmail.com>
2024-11-18 09:31:22 +0500

    upd

--- app/api/controllers/employee.py
+++ app/api/controllers/employee.py
@@ -3,7 +3,7 @@ from pydantic import PositiveInt
 
 from app import dto
 from app.api import schems
-from app.api.dependencies import dao_provider, AuthProvider, get_settings, get_superuser
+from app.api.dependencies import dao_provider, AuthProvider, get_settings, get_superuser, get_employee
 from app.config import Settings
 from app.infrastructure.database import HolderDao
 
@@ -16,9 +16,9 @@ router = APIRouter(prefix="/employee")
     response_model=dto.Employee,
 )
 async def create_employee(
-    employee: schems.RegisterEmployee,
-    dao: HolderDao = Depends(dao_provider),
-    settings: Settings = Depends(get_settings),
+        employee: schems.RegisterEmployee,
+        dao: HolderDao = Depends(dao_provider),
+        settings: Settings = Depends(get_settings),
 ) -> dto.Employee:
     auth = AuthProvider(settings=settings)
     if await dao.employee.get_employee(email=employee.email) is not None:
@@ -35,11 +35,16 @@ async def get_employees(dao: HolderDao = Depends(dao_provider)) -> list[dto.Empl
     return await dao.employee.get_all()
 
 
+@router.get(path="/me", description="Get me", response_model=dto.Employee)
+async def get_me(employee: dto.Employee = Depends(get_employee)) -> dto.Employee:
+    return employee
+
+
 @router.get(
     path="/{employee_id}", description="Get employee", response_model=dto.Employee
 )
 async def get_employee(
-    employee_id: PositiveInt = Path(), dao: HolderDao = Depends(dao_provider)
+        employee_id: PositiveInt = Path(), dao: HolderDao = Depends(dao_provider)
 ) -> dto.Employee:
     employee = await dao.employee.get_one(employee_id=employee_id)
     if employee is None:
@@ -73,7 +78,7 @@ async def get_employee(
     description="Delete employee",
 )
 async def delete_employee(
-    employee_id: PositiveInt = Path(), dao: HolderDao = Depends(dao_provider)
+        employee_id: PositiveInt = Path(), dao: HolderDao = Depends(dao_provider)
 ):
     employee = await dao.employee.get_one(employee_id=employee_id)
     if employee is None:
